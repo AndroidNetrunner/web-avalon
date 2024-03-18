957e3b4a0a5876059341d962c8ccd3159623e329
AndroidNetrunner <42410705+AndroidNetrunner@users.noreply.github.com>
2024-03-18 21:02:39 +0900

    [FIX] getAnalytics를 import하지 않은 문제 수정

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
