8e26d69d27965863b0aeec9c59b3b1015c5b0ad5
김병윤 <byungyoon-kim@gimbyeong-yun-ui-noteubug.local>
2024-05-27 18:16:44 +0900

    [FIX] 악의 하수인 정보 멘트 수정

--- src/components/RoleDescription.tsx
+++ src/components/RoleDescription.tsx
@@ -13,25 +13,26 @@ const roleToVisibleRoles: Partial<Record<Role, Role[]>> = {
 };
 
 const roleToDescription = {
-  멀린: "당신의 눈에 보이는 악의 하수인은...",
-  "악의 하수인": "당신의 눈에 보이는 악의 하수인은...",
-  암살자: "당신의 눈에 보이는 악의 하수인은...",
-  모르가나: "당신의 눈에 보이는 악의 하수인은...",
-  모드레드: "당신의 눈에 보이는 악의 하수인은...",
-  퍼시발: "당신의 눈에 보이는 멀린 후보는...",
+  멀린: "악의 하수인: ",
+  "악의 하수인": "악의 하수인: ",
+  암살자: "악의 하수인: ",
+  모르가나: "악의 하수인: ",
+  모드레드: "악의 하수인: ",
+  퍼시발: "멀린 후보: ",
   "선의 세력": "멀린을 도와 미션을 성공시켜 승리하세요!",
   오베론: "당신과 다른 악의 하수인들은 서로를 모릅니다.",
 };
 
 const getVisiblePlayers = (players: Player[], role: Role, myUserId: string) => {
   const visibleRoles = roleToVisibleRoles[role] || [];
-  return players
+  const visiblePlayers = players
     .filter(
       (player) =>
         player.userId !== myUserId && visibleRoles.includes(player.role)
     )
     .map((player) => player.username)
     .join(", ");
+  return visiblePlayers;
 };
 
 const getDescriptions = (role: Role) => {
@@ -47,12 +48,11 @@ const RoleDescription = ({ role }: { role: Role }) => {
 
   return (
     <>
-      <p>{description}</p>
       <p>
+        {description}
         <span style={{ fontWeight: "bold" }}>
           {getVisiblePlayers(players, role, myUserId)}
         </span>
-        입니다!
       </p>
     </>
   );
