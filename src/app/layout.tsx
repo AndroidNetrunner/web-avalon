38d03e84ff632008635c7891655414e930fdac46
김병윤 <byungyoon-kim@gimbyeong-yun-ui-noteubug.local>
2026-01-01 14:54:33 +0900

    [ADD] Google Adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1473493437844810"
+          crossOrigin="anonymous"
+        />
+      </head>
       <title>레지스탕스 아발론</title>
       <link rel="icon" href="/favicon.ico" />
       <body className={inter.className}>{children}</body>
